95e2556630396d770cabafd43798991910a7c30a
HADDOWJ(785007) <HADDOWJ(785007)@DESKTOP-TQGSA3F>
2016-12-03 18:03:04 +0000

    Add project files.

---
+++ Coursework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Coursework.Startup))]
+namespace Coursework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
